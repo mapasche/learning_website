332df24f5048e26c5f65b2ea1d8b06911b099c69
mpasche <martin.pasche@uc.cl>
2023-09-28 00:05:33 +0200

    html

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
+#Los viws son para conectar los models con los templates
 
 def post_list (request):
-    return render(request, 'blog/post_list.html', {})
+    #lte -> less than or equal
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts' : posts})
