3d8d45aa4bef164dfd6f15720e063e0dd91a2b3f
mpasche <martin.pasche@uc.cl>
2023-10-23 21:03:38 +0200

    tutorial finished without db postgre

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,11 @@ from .models import Post
 from django.utils import timezone
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 #Los viws son para conectar los models con los templates
+
 
 def post_list (request):
     #lte -> less than or equal
@@ -17,6 +19,22 @@ def post_detail (request, pk):
     post = get_object_or_404(Post, pk = pk)
     return render(request, 'blog/post_detail.html', {'post' : post})
 
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -33,6 +51,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form' : form})
 
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
